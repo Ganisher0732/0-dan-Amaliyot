474fedc51560d8367a66c31c5a487d06e0cad631
Ganisher <ganisherbaxtiyarov0732@gmail.com>
2025-11-11 16:04:29 +0500

    d

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
   const firstname = msg.chat.first_name;
+  const rasmferrari = "ferrari.webp";
 
   if (text === "/start") {
     await bot.sendMessage(
@@ -47,10 +48,31 @@ bot.on("message", async (msg) => {
     await bot.sendMessage(chatId, "Sport Car bo'limi tanlandi.", {
         reply_markup: {
             keyboard: [
-                
+                [{ text: "Ferrari"} , { text: "Lamborghini"}],
+                [{ text: "BMW M3"}],
+                [{ text: "⬅️ Orqaga"}],
+            ],
+        },
+    });
+  } else if (text === "Ferrari") {
+    await bot.sendMessage(chatId, "Ferrari bo'limi tanlandi", {
+        reply_markup: {
+            keyboard: [
+                [{ text: "Narx"} , { text: "Rasmlar"}],
             ]
         }
     });
-
-  } 
+  } else if (text === "Narx") {
+    await bot.sendMessage(chatId, "Ferrari narxi: $200,000",{
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: "Sotib olish"}]
+            ]
+        } 
+    });
+  } else if (text === "Sotib olish") {
+    await bot.sendMessage(chatId, "Siz sotib olish tugmasini bosdingiz. Tez orada siz bilan bog'lanamiz!");
+  } else if (text === "Rasmlar") {
+     
+  }
 });
